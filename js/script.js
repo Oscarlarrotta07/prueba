827ed223c615c469cc6f565336c593c0d0496df0
Oscar Larrotta <oscarlarrotta@hotmail.com>
2019-07-21 19:57:17 -0500

    cambios

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 function siguiente() {
  $('.bienvenida').addClass('inactivo'); 
  $('.encuesta').addClass('activo'); 
-
+ $('.btn_siguiente').addClass('inactivo'); 
+ $('.btn_siguiente_video').addClass('activo');
 }
 
+function siguientePos() {
+    $('.encuesta').addClass('inactivo');
+}
