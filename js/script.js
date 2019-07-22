8c40bf9e68be7cdd98926e08650c6645948fd302
Oscar Larrotta <oscarlarrotta@hotmail.com>
2019-07-21 21:02:51 -0500

    creacion alertas

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
 function siguiente() {
- $('.bienvenida').addClass('inactivo'); 
- $('.encuesta').addClass('activo'); 
- $('.btn_siguiente').addClass('inactivo'); 
- $('.btn_siguiente_video').addClass('activo');
+    $('.bienvenida').addClass('inactivo');
+    $('.encuesta').addClass('activo');
+    $('.btn_siguiente').addClass('inactivo');
+    $('.btn_siguiente_video').addClass('activo');
 }
 
 function siguienteVideo() {
-    $('.encuesta').removeClass('activo');
-    $('.video').addClass('activo');
-    $('.btn_siguiente_video').removeClass('activo');
-    $('.btn_siguiente_2').addClass('activo');
+    var r = confirm("Esta seguro de seguir.");
+    if (r == true) {
+        $('.encuesta').removeClass('activo');
+        $('.video').addClass('activo');
+        $('.btn_siguiente_video').removeClass('activo');
+        $('.btn_siguiente_2').addClass('activo');
+    }
+    else {
+        
+    }
 
 }
 
 function siguientePos() {
-    $('.video').removeClass('activo');
-    $('.btn_siguiente_2').removeClass('activo');
-    $('.encuestaPos').addClass('activo'); 
+    var r = confirm("Esta seguro de seguir.");
+    if ( r == true) {
+        $('.video').removeClass('activo');
+        $('.btn_siguiente_2').removeClass('activo');
+        $('.encuestaPos').addClass('activo');  
+    }
+    else{
+        
+    } 
 
 }
