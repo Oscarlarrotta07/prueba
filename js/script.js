4d5d03121d0bb012693bc2cb62aeaf4f0a9bf815
Oscar Larrotta <oscarlarrotta@hotmail.com>
2019-07-24 19:56:25 -0500

    cambio a solo una encuesta

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 function siguiente() {
     $('.bienvenida').addClass('inactivo');
-    $('.encuesta').addClass('activo');
+    $('.video').addClass('activo');
     $('.btn_siguiente').addClass('inactivo');
     $('.btn_siguiente_video').addClass('activo');
 }
 
 function siguienteVideo() {
-    var r = confirm("¿Esta seguro que envio el formulario?.");
+    var r = confirm("¿Esta seguro de haber visto el video completo?.");
     if (r == true) {
-        $('.encuesta').removeClass('activo');
-        $('.video').addClass('activo');
+        $('.encuesta').addClass('activo');
+        $('.video').removeClass('activo');
         $('.btn_siguiente_video').removeClass('activo');
-        $('.btn_siguiente_2').addClass('activo');
+        $('.finalizar').addClass('activo');
     }
     else {
-        
+
     }
 
 }
 
-function siguientePos() {
-    var r = confirm("¿Esta seguro de haber visto todo el video?.");
-    if ( r == true) {
-        $('.video').removeClass('activo');
-        $('.btn_siguiente_2').removeClass('activo');
-        $('.encuestaPos').addClass('activo');  
+function mensajeDespedida() {
+    
+    var r = confirm("¿Esta seguro de haber enviado el formulario?.");
+    if (r == true) {
+        $('.encuesta').removeClass('activo');
+    $('.despedida').addClass('activo');
+    $('.finalizar').removeClass('activo');
     }
-    else{
+    else {
 
-    } 
-
+    }
 }
